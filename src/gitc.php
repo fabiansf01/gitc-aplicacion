f0b45b41362c9eb990efa002552ae5759b5023f4
fabiansf01 <110618996+fabiansf01@users.noreply.github.com>
2023-01-17 20:36:41 -0300

    img

---
+++ src/gitc.php
@@ -0,0 +1,6 @@
+<?php
+echo  "https://www.fpsoft.com.ar" . "br";
+echo  "fabianperaltasfco@gmail.com.ar"  . "br";
+echo  "+54 9 3564 560635 " . "br";
+echo  " GITC - GESTION INTEGRAL DE TARJETAS DE COMPRA Y CREDITO  - DISEÑADO CON VISUAL FOX + Mysql"
+?>
